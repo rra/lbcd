948bb90190f59ed992723aab55d7fc02044f00ef
Larry Schwimmer <opusl@stanford.edu>
1996-10-22 12:43:26 +0000

    Initial revision

---
+++ lbcd.h
@@ -0,0 +1,16 @@
+#ifndef __LBCD_H__
+#define __LBCD_H__
+
+/* kernel.c */
+extern int kernel_open();     
+extern int kernel_close();
+extern int kernel_getload(double *l1, double *l5, double *l15);
+extern int kernel_getboottime(time_t *boottime);
+extern int kernel_read(off_t where, void *dest, int dest_len);
+
+/* get_user.c */
+extern int get_user_stats(int *total,
+			  int *unique, int *onconsole, time_t *user_mtime);
+
+
+#endif /* __LBCD_H__ */
