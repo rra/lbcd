079afc5f95cb0750ae4ce44683f05112a46e8ce8
Larry Schwimmer <opusl@stanford.edu>
1997-10-18 23:56:42 +0000

    ANSIfy source.  Add define defaults.

--- lbcd.h
+++ lbcd.h
@@ -1,16 +1,33 @@
 #ifndef __LBCD_H__
 #define __LBCD_H__
 
+/*
+ * Includes
+ */
+#include <sys/types.h>
+
+/*
+ * Defines
+ */
+#ifndef PROGNAME
+#define PROGNAME "lbcd"
+#endif
+
+#ifndef PID_FILE
+#define PID_FILE "/etc/lbcd.pid"
+#endif
+
+/*
+ * Prototypes
+ */
+
 /* kernel.c */
-extern int kernel_open();     
-extern int kernel_close();
 extern int kernel_getload(double *l1, double *l5, double *l15);
 extern int kernel_getboottime(time_t *boottime);
-extern int kernel_read(off_t where, void *dest, int dest_len);
 
 /* get_user.c */
-extern int get_user_stats(int *total,
-			  int *unique, int *onconsole, time_t *user_mtime);
+extern int get_user_stats(int *total, int *unique,
+			  int *onconsole, time_t *user_mtime);
 
 /* tmp_free.c */
 extern int tmp_free(void);
