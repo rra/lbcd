b88a6cd4775f59a21a95e4a8a815c6fded4a1b50
Larry Schwimmer <opusl@stanford.edu>
1997-10-27 08:12:32 +0000

    change proto_ to lbcd_; C++ support

--- proto_server.h
+++ proto_server.h
@@ -1,17 +1,28 @@
-#ifndef LBCD_PROTOCOL_SERVER_H
-#define LBCD_PROTOCOL_SERVER_H
+#ifndef LBCD_SERVER_H
+#define LBCD_SERVER_H
 
 #include "protocol.h"
 
 /* protocol.c */
 
-void proto_pack_lb_info(P_LB_RESPONSE *lb);
-int proto_recv_udp(int s, 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lbcd_pack_info(P_LB_RESPONSE *lb);
+int lbcd_recv_udp(int s, 
                    struct sockaddr_in *cli_addr, int * cli_len,
                    char *mesg, int max_mesg);
 
-int proto_send_status(int s, 
+int lbcd_send_status(int s, 
                    struct sockaddr_in *cli_addr, int cli_len,
                    P_HEADER *request_header,
                    p_status_t pstat);
+
+int lbcd_print_load(void);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
