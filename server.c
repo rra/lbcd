397012b66fa4915a750a99f21b4637979e5416a0
Larry Schwimmer <opusl@stanford.edu>
1997-10-27 08:15:14 +0000

    start of support for version 3 of the protocol change proto_ namespace to lbcd_

--- server.c
+++ server.c
@@ -9,7 +9,7 @@
 #include "proto_server.h"
 
 int
-proto_recv_udp(int s, 
+lbcd_recv_udp(int s, 
 	       struct sockaddr_in *cli_addr, int * cli_len,
 	       char *mesg, int max_mesg)
 {
@@ -35,24 +35,20 @@ proto_recv_udp(int s,
 
   switch(ph->version) {
   case 3:
-    /* Basic Extended Version */
-    if (ph->status > 0) {
-      util_log_error("extended protcol 3 not supported yet");
-      proto_send_status(s, cli_addr, *cli_len,ph,status_proto_version);
-    }
+    /* Client-supplied load protocol */
     break;
   case 2:
-    /* Original Protocol */
+    /* Original protocol */
     break;
   default:
     util_log_error("protocol version unsupported: %d", ph->version);
-    proto_send_status(s, cli_addr, *cli_len,ph,status_proto_version);
+    lbcd_send_status(s, cli_addr, *cli_len,ph,status_lbcd_version);
     return 0;
   }
 
   if (ph -> status != status_request) {
     util_log_error("expecting request, got %d",ph->status);
-    proto_send_status(s, cli_addr, *cli_len,ph,status_proto_error);
+    lbcd_send_status(s, cli_addr, *cli_len,ph,status_lbcd_error);
     return 0;
   }
   
@@ -60,7 +56,24 @@ proto_recv_udp(int s,
 }
 
 void
-proto_pack_lb_info(P_LB_RESPONSE *lb)
+lbcd_set_load(P_LB_RESPONSE *lb)
+{
+  /* FIXME: fill in below */
+#if 1
+  lb->services = 0;
+  lb->host_weight = 0;
+  lb->host_incr = 0;
+#else
+  int i;
+  set_load(lb,0);
+  for (i = 1; i < lb->services; i++) {
+    set_load(lb,i);
+  }
+#endif
+}
+
+void
+lbcd_pack_lb_info(P_LB_RESPONSE *lb)
 {
   double l1,l5,l15;
   time_t bt,ct;
@@ -73,7 +86,14 @@ proto_pack_lb_info(P_LB_RESPONSE *lb)
   time(&ct);
   lb->current_time = htonl(ct);
 
+  /**/
+#if 0
+  if (round_robin && lb->h->version < 3) {
+
+  } else {
+#endif
   kernel_getload(&l1,&l5,&l15);
+
   lb->l1 = htons((u_short)(l1*100));
   lb->l5 = htons((u_short)(l5*100));
   lb->l15 = htons((u_short)(l15*100));
@@ -83,24 +103,30 @@ proto_pack_lb_info(P_LB_RESPONSE *lb)
   lb->tot_users = htons(tu);  
   lb->uniq_users = htons(uu);
   lb->on_console = oc;
+#if 0
+  }
+#endif
+  /**/
   lb->user_mtime = htonl(umtime);
   lb->reserved = 0;
-  lb->tmp_full = tmp_full();
-  /* FIXME: fill in below */
-  lb->tmpdir_full = tmp_full();
-  lb->pad = htons(0);
-  lb->host_weight = htonl(0);
-  lb->host_incr = htonl(0);
+  lb->tmp_full = tmp_full("/tmp");
+#ifdef P_tmpdir
+  lb->tmpdir_full = tmp_full(P_tmpdir);
+#else
+  lb->tmpdir_full = lb->tmp_full;
+#endif
+
+  lbcd_set_load(lb);
 }
 
 int
-proto_send_status(int s, 
+lbcd_send_status(int s, 
 		  struct sockaddr_in *cli_addr, int cli_len,
 		  P_HEADER *request_header,
 		  p_status_t pstat)
 {
   P_HEADER header;
-  header.version= htons(PROTO_VERSION);
+  header.version= htons(LBCD_VERSION);
   header.id     = htons(request_header->id);
   header.op     = htons(request_header->op);
   header.status = htons(pstat);
@@ -113,14 +139,12 @@ proto_send_status(int s,
   return 0;
 }
 
-#ifdef MAIN
-
 int
-main(int argc, char *argv[])
+lbcd_print_load(void)
 {
   P_LB_RESPONSE lb;
 
-  proto_pack_lb_info(&lb);
+  lbcd_pack_lb_info(&lb);
 
   printf("  lb.l1 = %d\n",  ntohs(lb.l1));
   printf("  lb.l5 = %d\n",  ntohs(lb.l5));
@@ -132,6 +156,14 @@ main(int argc, char *argv[])
   printf("  lb.uniq_users = %d\n",  ntohs(lb.uniq_users));
   printf("  lb.on_console = %d\n",  lb.on_console);
   printf("  lb.tmp_full = %d\n",  lb.tmp_full);
+  printf("  lb.tmpdir_full = %d\n",  lb.tmpdir_full);
 }
 
+#ifdef MAIN
+int
+main(int argc, char *argv[])
+{
+  lbcd_print_load();
+  return 0;
+}
 #endif
