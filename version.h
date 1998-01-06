b8bcf5ea924f4136bce46313d3217c081aec8e88
Larry Schwimmer <opusl@stanford.edu>
1998-01-06 18:06:59 +0000

    Initial revision

---
+++ version.h
@@ -0,0 +1,7 @@
+#ifndef __lbcd_version_h__
+#define __lbcd_version_h__
+
+/* Program version string -- major and protocol should match */
+#define VERSION "3.0.0beta3"
+
+#endif /* __lbcd_version_h__ */
