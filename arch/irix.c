bbed2b008104b57de6a4c8cf883afe725c9e6e5f
Russ Allbery <rra@stanford.edu>
2000-01-11 16:15:12 +0000

    Rewritten using the sysmp() method (rather than nlist()) from the XEmacs sources.  Get the boottime out of utmpx instead of out of the kernel.

--- arch/irix.c
+++ arch/irix.c
@@ -1,131 +1,115 @@
-/*
- * lbcd kernel code for IRIX
- */
+/* irix.c -- lbcd kernel code for IRIX.
+   $Id$
 
-#include <stdio.h>
+   Written by Russ Allbery <rra@stanford.edu>
+   Copyright 2000 Board of Trustees, Leland Stanford Jr. University
+
+   Based on src/getloadavg.c from XEmacs 21.1.7.  Use utmpx to get the
+   system boot time as the method used to get kernel addresses doesn't seem
+   to work for the time of the last reboot. */
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <nlist.h>
-#include <sys/param.h>
+#include <sys/sysmp.h>
 #include <sys/time.h>
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-#include "lbcd.h"
+#include <utmpx.h>
 
-#define C_KMEM "/dev/kmem"
-#define C_VMUNIX "/unix"
-#define FSCALE	1024.0
-
-static struct nlist nl[] = {
-  { "avenrun" },         /* load average */
-#define K_NL_LOADAVERAGE 0
-  { "boottime" },         /* boottime */
-#define K_NL_BOOTTIME 1
-  { 0 }
-};
-
-static int kd = -1;
+/* Whether we've opened the kernel already. */
 static int kernel_init = 0;
 
-static int
-kernel_open(void)
-{
-  if ((kd = open(C_KMEM, O_RDONLY)) < 0) {
-    util_log_error("open: %%m");
-    exit(1);
- }
-
- if (nlist(C_VMUNIX, nl) !=0) {
-    util_log_error("nlist error");
-    exit(1);
- }
-
-  kernel_init = 1;
-  return 0;
-}
+/* The file descriptor of /dev/kmem, which we hold open. */
+static int kmem_fd = -1;
 
 int
 kernel_close(void)
 {
-  if (! kernel_init) return 0;
-  return close(kd);
-}
-
-static int
-kernel_read(off_t where, void *dest, int dest_len)
-{
-   int stat;
-
-   if (lseek(kd,where,SEEK_SET)==-1) {
-        util_log_error("can't lseek in %s: %%m",C_KMEM);
-        exit(1);
-   }
-
-   stat = read(kd, dest, dest_len);
-   if (stat==-1) {
-     util_log_error("read : %%m");
-     exit(1);
-   }   
-   return stat;
+    if (!kernel_init) return 0;
+    return close(kmem_fd);
 }
 
 int
-kernel_getload(double *l1, double *l5, double *l15)
+kernel_getload(double *load1, double *load5, double *load15)
 {
-  long kern_avenrun[3];
+    int ldav_off, status, load[3];
+    static long offset = -1;
 
-  if (!kernel_init) kernel_open();
+    /* If necessary, open the system memory map. */
+    if (!kernel_init) {
+        kmem_fd = open("/dev/kmem", O_RDONLY);
+        if (kmem_fd < 0) {
+            util_log_error("open of /dev/kmem failed: %%m");
+            exit(1);
+        }
+        kernel_init = 1;
+    }
 
-  if (nl[K_NL_LOADAVERAGE].n_type==0) return -1;
+    /* Retrieve the kernel offset of the load average. */
+    if (offset == -1) {
+        ldav_off = sysmp(MP_KERNADDR, MPKA_AVENRUN);
+        if (ldav_off == -1) {
+            util_log_error("sysmp: %%m");
+            exit(1);
+        }
+        offset = (long) ldav_off & 0x7fffffff;
+    }
+    
+    /* Try to read the load. */
+    if (lseek(kmem_fd, offset, 0) == -1L) {
+        util_log_error("can't lseek in /dev/kmem: %%m");
+        exit(1);
+    }
+    status = read(kmem_fd, (char *) load, sizeof(load));
+    if (status != sizeof(load)) {
+        util_log_error("read only %d bytes: %%m", status);
+        exit(1);
+    }
 
-  if (kernel_read( (off_t)nl[K_NL_LOADAVERAGE].n_value, 
-		   (char *) kern_avenrun,sizeof(kern_avenrun))==-1) { 
-    util_log_error("kernel loadaverage read error: %%m");
-    return -1;
-  }
+    *load1  = (double) load[0] / 1000.0;
+    *load5  = (double) load[1] / 1000.0;
+    *load15 = (double) load[2] / 1000.0;
 
-  *l1 = (double)kern_avenrun[0]/FSCALE;
-  *l5 = (double)kern_avenrun[1]/FSCALE;
-  *l15 =(double)kern_avenrun[2]/FSCALE;
-
-  return 0;
+    return 0;
 }
 
 int
 kernel_getboottime(time_t *boottime)
 {
+    struct utmpx query;
+    struct utmpx *result;
 
-  struct timeval boot;
-
-  if (!kernel_init) kernel_open();
-
-  if (nl[K_NL_BOOTTIME].n_type==0) return -1;
-
-  if (kernel_read( (off_t)nl[K_NL_BOOTTIME].n_value, 
-                       (char *) &boot,sizeof(boot))==-1) {
-     util_log_error("kernel boottime read error: %%m"); 
-      return -1;
-  }
-
-  *boottime = boot.tv_sec;
-
-  return 0;
+    query.ut_type = BOOT_TIME;
+    setutxent();
+    result = getutxid(&query);
+    endutxent();
+    if (!result) {
+        util_log_error("getutxid failed to find BOOT_TIME");
+        return -1;
+    }
+    *boottime = result->ut_tv.tv_sec;
+    return 0;
 }
 
 #ifdef MAIN
+
+#include <stdio.h>
+#include <time.h>
+
 int
-main()
+main(void)
 {
-  double l1,l5,l15;
-  time_t boottime;
+    double load1, load5, load15;
+    time_t boottime;
 
-  if (kernel_getload(&l1,&l5,&l15) == 0) {
-    printf("load %.02f %.02f %.02f\n",l1,l5,l15);
-  }
-  if (kernel_getboottime(&boottime) == 0) {
-    printf("booted at %s",ctime(&boottime));
-  }
+    if (kernel_getload(&load1, &load5, &load15) == 0) {
+        printf("load %.02f %.02f %.02f\n", load1, load5, load15);
+    }
+    if (kernel_getboottime(&boottime) == 0) {
+        printf("booted at %s", ctime(&boottime));
+    }
 
-  return 0;
+    return 0;
 }
-#endif
+
+#endif /* MAIN */
